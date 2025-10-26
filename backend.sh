3eb52fb22792d4c5e0f50ee5232bcd8570a436bb
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2025-10-26 21:04:37 +0530

    expense project through shell

---
+++ backend.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/backend-logs"
+LOG=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$LOG-$TIMESTAMP.log"
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 : $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 : $G SUCCESS $N"
+    fi
+}
+CHECK_ROOT (){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: Please run this script as root user"
+        exit 1
+    fi
+}
+echo "This script runs at: $TIMESTAMP"
+mkdir -p $LOGS_FOLDER
+CHECK_ROOT
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "disable nodejs default version"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "enable required nodejs version"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "install nodejs"
+useradd expense &>>$LOG_FILE
+VALIDATE $? "useradd expense"
+mkdir -p /app &>>$LOG_FILE
+VALIDATE $? "create directory"
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "downloading backend code"
+cd /app &>>$LOG_FILE
+VALIDATE $? "changing app directory"
+npm install &>>$LOG_FILE
+VALIDATE $? "install npm dependencies"
+copy /home/ec2-user/backend.service /etc/systemd/system/backend.service &>>$LOG_FILE
+VALIDATE $? "copying the backend file"
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "daemon reload"
+systemctl start backend &>>$LOG_FILE
+VALIDATE $? "start backend"
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "enable backend"
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "install mysql"
+mysql -h 172.31.29.71 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "load schema"
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "restart backend"
